a43aa3bf8c86758d48dff4a48d303f1275ff2aa9
Jon Tice <chilifreez@aol.com>
2024-08-23 16:30:53 -0400

    Create listings form created

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,6 +4,8 @@ import { useNavigate, Link } from "react-router-dom"
 import { updateDoc, doc } from "firebase/firestore"
 import {db} from '../firebase.config'
 import { toast } from "react-toastify"
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
 
 function Profile() {
 
@@ -75,6 +77,12 @@ function Profile() {
                     <input type="text" id="email" className={!changeDetails ? 'profileEmail' : 'profileEmailActive'} disabled={!changeDetails} value={email} onChange={onChange} />
                 </form>
             </div>
+
+            <Link to='/create-listing' className="createListing">
+                <img src={homeIcon} alt="home" />
+                <p>Sell or rent your home</p>
+                <img src={arrowRight} alt="Arrow Right" />
+            </Link>
         </main>
     </div>
 }
