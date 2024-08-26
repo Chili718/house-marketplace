9a304d14fb7664e8b1a4902515c30d1fc9635a64
Jon Tice <chilifreez@aol.com>
2024-08-26 12:18:51 -0400

    Create listing functionality completed

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,7 +1,12 @@
 import { useState, useEffect, useRef } from "react"
 import {getAuth, onAuthStateChanged} from 'firebase/auth'
+import {getStorage, ref, uploadBytesResumable, getDownloadURL} from 'firebase/storage'
+import {db} from '../firebase.config'
+import {addDoc, collection, serverTimestamp} from 'firebase/firestore'
 import { useNavigate } from "react-router-dom"
 import Spinner from '../components/Spinner'
+import { toast } from "react-toastify"
+import {v4 as uuidv4} from 'uuid'
 
 function CreateListing() {
 
@@ -47,9 +52,106 @@ function CreateListing() {
 
     }, [isMounted])
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault()
-        console.log(formData)
+
+        setLoading(true)
+        
+        if(+discountedPrice >= +regularPrice){
+            setLoading(false)
+            toast.error('Discounted price needs to be less than regular price')
+            return
+        }
+
+        if(images.length > 6) {
+            setLoading(false)
+            toast.error('Max 6 images')
+            return
+        }
+
+        let geolocation = {}
+        let location
+
+        if(geolocationEnabled){
+
+        }else{
+            geolocation.lat = latitude
+            geolocation.lng = longitude
+            location = address
+        }
+
+        //store images in firebase
+        const storeImage = async (image) => {
+            if(image.size/1024 > 2000){//firebase will not take images greater than 2mb
+
+                toast.error('Image could not be uploaded due to exceeding file size of 2mb!')
+                return
+
+            }else{
+                return new Promise((resolve, reject) => {
+                    const storage = getStorage()
+                    const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+
+                    const storageRef = ref(storage, 'images/' + fileName)
+
+                    const uploadTask = uploadBytesResumable(storageRef, image)
+
+                    uploadTask.on('state_changed',
+                        (snapshot) => {
+
+                            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                            //console.log('Upload is ' + progress + '% done');
+                            switch (snapshot.state) {
+                                case 'paused':
+                                    console.log('Upload is paused');
+                                    break;
+                                case 'running':
+                                    console.log('Upload is running');
+                                    break;
+                            }
+                        },
+                        (error) => {
+                            reject(error)
+                        },
+                        () => {
+
+                            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                                resolve(downloadURL)
+                            });
+                        }
+                    );
+                })
+            }
+    
+        }
+
+        const imageUrls = await Promise.all(
+            [...images].map((image) => storeImage(image))
+        ).catch(() => {
+            setLoading(false)
+            toast.error('Images not uploaded')
+            return
+        })
+
+        const formDataCopy = {
+            ...formData,
+            imageUrls,
+            geolocation,
+            timestamp: serverTimestamp(),
+        }
+
+        delete formDataCopy.images
+        delete formDataCopy.address
+        location && (formDataCopy.location = location)
+        //if no offer then remove the discounted price from object
+        !formDataCopy.offer && delete formDataCopy.discountedPrice
+
+        const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
+        setLoading(false)
+
+        toast.success('Listing saved')
+        navigate(`/category/${formDataCopy.type}/${docRef.id}`)
+
     }
 
     const onMutate = (e) => {
