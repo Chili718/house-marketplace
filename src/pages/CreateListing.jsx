8b5a48ae1a8486cbbe7c624f3bceee2c2f5b852a
Jon Tice <chilifreez@aol.com>
2024-08-26 18:16:43 -0400

    Update functionality completed/other minor fixes

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -108,6 +108,8 @@ function CreateListing() {
                                 case 'running':
                                     console.log('Upload is running');
                                     break;
+                                default:
+                                    break;
                             }
                         },
                         (error) => {
